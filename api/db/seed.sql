01a2de76c29ec741bb9b01f5047425933cb5c02b
Rishabh <rbrawat98@gmail.com>
2021-01-05 17:26:02 +0000

    a bit of db and routes

--- api/db/seed.sql
+++ api/db/seed.sql
@@ -5,7 +5,13 @@ CREATE TABLE tele (
     title VARCHAR (200) NOT NULL,
     user VARCHAR(200) NOT NULL,
     body VARCHAR NOT NULL,
-)
+    ts DATE DEFAULT NOW()::DATE,
+);
 
+
+INSERT INTO tele (title, user, body)
+VALUES
+    ("It's been a long day", "RO", "it's been a long long day??!!!!"),
+    ("Let's GOO", "PAOLA", "LET'S WIN!!!!!!!!!!!!!!!!!!!!")
 
 
