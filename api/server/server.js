e3a1f22b88c7e2f5c63457f57a79f96e127016be
PaolaDM <orchidea.preziosa@gmail.com>
2021-01-06 10:01:14 +0000

    modified form api call  and server port

--- api/server/server.js
+++ api/server/server.js
@@ -6,5 +6,7 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 
+//server.listen(3000, () => console.log(`\nExpress departing now from port 3000!\n`))
 
+module.exports = server
 
